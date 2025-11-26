3e77cc5a54abfeef50fbb306934794bfdcc404ee
KRiSTALiK-sys <124252591+KRiSTALiK-sys@users.noreply.github.com>
2025-11-26 15:47:12 +0500

    Add files via upload

---
+++ TaskManagmentTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaskManagmentTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskManagmentTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cda66c7b-8ffc-4b17-b730-b5d2ddebb7c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaskManagmentTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManagementApp;
+
+namespace TaskManagmentTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+        private TaskManager taskManager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            taskManager = new TaskManager();
+        }
+
+        [TestMethod]
+        public void AddTask_ShouldAddTaskToList()
+        {
+            var task = new TaskItem { Title = "Test Task", Description = "Test Description", DueDate = DateTime.Now, IsCompleted = false };
+
+            taskManager.AddTask(task);
+            var tasks = taskManager.GetAllTasks();
+
+            Assert.AreEqual(1, tasks.Count, "Задача не была добавлена в список.");
+            Assert.AreEqual("Test Task", tasks[0].Title, "Заголовок задачи не совпадает с ожидаемым.");
+        }
+
+        [TestMethod]
+        public void DeleteTask_ShouldRemoveTaskFromList()
+        {
+            var task1 = new TaskItem { Title = "Task 1" };
+            var task2 = new TaskItem { Title = "Task 2" };
+            taskManager.AddTask(task1);
+            taskManager.AddTask(task2);
+
+            taskManager.RemoveTask(task1);
+            var tasks = taskManager.GetAllTasks();
+
+            Assert.AreEqual(1, tasks.Count);
+            Assert.AreEqual("Task 2", tasks[0].Title);
+        }
+
+        [TestMethod]
+        public void AddTask_NullTask_ShouldNotAddTask()
+        {
+            TaskItem task = null;
+
+            taskManager.AddTask(task);
+            var tasks = taskManager.GetAllTasks();
+
+            Assert.AreEqual(0, tasks.Count);
+        }
+    }
+}
